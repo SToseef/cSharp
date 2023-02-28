6a706ed4ced840ddd523522b20b0e111fb9c0911
Sohail Toseef <124055499+SToseef@users.noreply.github.com>
2023-02-28 17:09:10 +0000

    fileUpload

---
+++ HelloMars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloMars
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string myName = args[0];
+            Console.WriteLine("Hello Mars! from " + myName);
+            Console.ReadLine();
+        }
+    }   
+}
