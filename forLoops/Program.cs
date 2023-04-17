e5a24efb30054236ad9637132bd07f972d5273d8
Sohail Toseef <124055499+SToseef@users.noreply.github.com>
2023-04-17 14:02:27 +0100

    forLoops

---
+++ forLoops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GoTo
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            
+            Console.WriteLine("***** Checkers *****");
+            int[,] checkers = new int[4,4];
+            bool sqaureValue = true;
+
+            for (int i = 0; i < 4; i++)
+            {
+                PopulateCheckerRow(ref checkers, i, sqaureValue);
+                sqaureValue = !sqaureValue;
+            }
+
+            PrintCheckers(checkers);
+        }
+
+        static void PopulateCheckerRow (ref int[,] checks, int row, bool sv)
+        {
+            for (int col = 0; col < 4; col++)
+            {
+                checks[row, col] = Convert.ToInt32(sv);
+                sv = !sv;
+            }
+        }
+
+        static void PrintCheckers(int[,] checks)
+        {
+            for (int r = 0; r < 4; r++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    Console.Write($"{checks[r,c]}");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }   
+}
