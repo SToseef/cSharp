92798c2d3c83de407d49fb9ed06c3f163f001e7d
Sohail Toseef <124055499+SToseef@users.noreply.github.com>
2023-05-28 13:26:00 +0100

    If-Statement
    If-Statement test

---
+++ ConditionalBranch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConditionalBranch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int porchLumens = 800;
+            int moonLumens = 1200;
+            int neighborsPorch = 900;
+            //string flightPath = porchLumens > neighborsPorch ? "Head to porch" : neighborsPorch > moonLumens ? "visit neighbors" : "Fly to moon";
+
+/*                if (porchLumens > moonLumens)
+            {
+                flightPath = "Head to porch";
+                porchLumens = 0;
+            }
+            else if (neighborsPorch > moonLumens)
+            {
+                flightPath = "Vist neighbors";
+                neighborsPorch = 0;
+            }
+            else
+                flightPath = "Fly to moon"; */
+
+            Console.WriteLine(porchLumens > neighborsPorch ? "Head to porch" : neighborsPorch > moonLumens ? "visit neighbors" : "Fly to moon");
+        }
+    }
+}
